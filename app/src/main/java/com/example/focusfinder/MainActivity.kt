571f5baa5d079787ae018baab69a5eb30cf9103c
ethandoy <ethanvdoyle@icloud.com>
2022-12-03 11:25:08 -0500

    Started Menu, fixed dashboard.xml, fixed activity_main.xml

--- app/src/main/java/com/example/focusfinder/MainActivity.kt
+++ app/src/main/java/com/example/focusfinder/MainActivity.kt
@@ -2,12 +2,32 @@ package com.example.focusfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Ethan Comment
+        findViewById<BottomNavigationView>(R.id.bottom_menu).setOnItemSelectedListener {
+            if (it.itemId == R.id.dashboard) {
+                findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_dashboard)
+                return@setOnItemSelectedListener true
+            } else if (it.itemId == R.id.med_tracker) {
+                findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_medicineHome)
+                return@setOnItemSelectedListener true
+            } else if (it.itemId == R.id.task_tracker) {
+                findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_taskHome)
+                return@setOnItemSelectedListener true
+            } else if (it.itemId == R.id.timer) {
+                findNavController(R.id.nav_host_fragment).navigate(R.id.action_global_timer)
+                return@setOnItemSelectedListener true
+            } else {
+                return@setOnItemSelectedListener true
+
+            }
+        }
+
     }
 }
