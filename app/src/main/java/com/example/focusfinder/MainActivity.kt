897b62167807c016cc54ca916bd6be56871a1511
ethandoy <ethanvdoyle@icloud.com>
2022-12-06 15:34:35 -0500

    UI Bug Fixes

--- app/src/main/java/com/example/focusfinder/MainActivity.kt
+++ app/src/main/java/com/example/focusfinder/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.focusfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
