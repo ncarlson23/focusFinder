2b2c857b8aaa332aace46747ea412b6d292b4976
ethandoy <ethanvdoyle@icloud.com>
2022-12-07 14:49:01 -0500

    setup DAO for Task and Medicine

--- app/src/main/java/com/example/focusfinder/MedicineDAO.kt
+++ app/src/main/java/com/example/focusfinder/MedicineDAO.kt
@@ -1,7 +1,19 @@
 package com.example.focusfinder
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface MedicineDAO {
+
+    // add a single task
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(medicine: Medicine)
+
+    // return list of all tasks
+    @Query("SELECT * FROM medicineDB")
+    fun getTaskMedicineFromDB():List<Medicine>
+
 }
--- app/src/main/java/com/example/focusfinder/TaskDAO.kt
+++ app/src/main/java/com/example/focusfinder/TaskDAO.kt
@@ -4,9 +4,13 @@ import androidx.room.*
 
 @Dao
 interface TaskDAO {
+    // add a single task
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(task:Task)
 
-//    @Query ("SELECT * FROM taskDB")
-//    fun getTaskListFromDB():List<Task>
+    // return list of all tasks
+    @Query ("SELECT * FROM taskDB")
+    fun getTaskListFromDB():List<Task>
 
 
 
